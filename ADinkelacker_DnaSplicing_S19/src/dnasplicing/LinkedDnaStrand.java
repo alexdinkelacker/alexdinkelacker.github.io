bf0f60528396968996f7bad28d013d904f3ffd5d
Alex Dinkelacker <55370203+alexdinkelacker@users.noreply.github.com>
2020-04-21 23:19:48 -0700

    Add files via upload

---
+++ ADinkelacker_DnaSplicing_S19/src/dnasplicing/LinkedDnaStrand.java
@@ -0,0 +1,59 @@
+package dnasplicing;
+
+import static sbcc.Core.*;
+import static java.lang.System.*;
+import static org.apache.commons.lang3.StringUtils.*;
+
+public class LinkedDnaStrand {
+
+	public LinkedDnaStrand(String dna) {
+
+	}
+
+
+	public static void main(String[] args) {
+
+	}
+
+
+	public Object getFirstDnaNode() {
+
+		return null;
+	}
+
+
+	public Object getDnaNodeCount() {
+
+		return null;
+	}
+
+
+	public Object getAppendCount() {
+
+		return null;
+	}
+
+
+	public Object getNucleotideCount() {
+
+		return null;
+	}
+
+
+	public DnaStrand cutSplice(String enzyme, String splicee) {
+
+		return null;
+	}
+
+
+	public void append(String dnaToAppend) {
+
+	}
+
+
+	public LinkedDnaStrand createReversedDnaStrand() {
+
+		return null;
+	}
+
+}
